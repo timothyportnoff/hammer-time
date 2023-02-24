e6bdc257e09b5daad757891d0a628822553a5af3
= <=>
2023-02-23 21:43:20 -0800

    Added another averaging_measure and a boolean function

--- sonar.py
+++ sonar.py
@@ -9,6 +9,7 @@
 #
 # Author : Matt Hawkins
 # Date   : 28/01/2013
+# Editor : Lara
 
 # -----------------------
 # Import required Python libraries
@@ -21,17 +22,17 @@ import RPi.GPIO as GPIO
 # -----------------------
 
 def measure():
-  # This function measures a distance
-  GPIO.output(GPIO_TRIGGER, True)
-  time.sleep(0.00001)
-  GPIO.output(GPIO_TRIGGER, False)
-  start = time.time()
-
-  while GPIO.input(GPIO_ECHO)==0:
+    # This function measures a distance
+    GPIO.output(GPIO_TRIGGER, True)
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
     start = time.time()
 
+  while GPIO.input(GPIO_ECHO)==0:
+      start = time.time()
+
   while GPIO.input(GPIO_ECHO)==1:
-    stop = time.time()
+      stop = time.time()
 
   elapsed = stop-start
   distance = (elapsed * 34300)/2
@@ -39,30 +40,56 @@ def measure():
   return distance
 
 def measure_average():
-  # This function takes 3 measurements and
-  # returns the average.
-  distance1=measure()
-  time.sleep(0.1)
-  distance2=measure()
-  time.sleep(0.1)
-  distance3=measure()
-  distance = distance1 + distance2 + distance3
-  distance = distance / 3
-  return distance
+    # This function takes 3 measurements and returns the average.
+    distance1=measure()
+    time.sleep(0.1)
+    distance2=measure()
+    time.sleep(0.1)
+    distance3=measure()
+    distance = distance1 + distance2 + distance3
+    distance = distance / 3
+    return distance
 
-# -----------------------
+def measure_better_distance():
+    # This function takes 3 measurements and returns the average.
+    distance1=measure()
+    time.sleep(0.05)
+    distance2=measure()
+    time.sleep(0.05)
+    distance3=measure()
+    time.sleep(0.05)
+    distance4=measure()
+    time.sleep(0.05)
+    distance5=measure()
+    distance = distance1 + distance2 + distance3 + distance4 + distance5
+    distance = distance / 5
+    return distance
+
+# This function takes in a distance and returns a boolean.
+def within_sonar_range(CM):
+    if measure_better_distance() < CM return True       #Try 1
+    else if measure_better_distance() < CM return True  #Try 2
+    else if measure_better_distance() < CM return True  #Try 3
+    else return False
+
+    # -----------------------
 # Main Script
 # -----------------------
 
-# Use BCM GPIO references
-# instead of physical pin numbers
+# Use BCM GPIO references instead of physical pin numbers
 GPIO.setmode(GPIO.BCM)
 
 # Define GPIO to use on Pi
-GPIO_TRIGGER = 17
-GPIO_ECHO    = 27
+GPIO_TRIGGER = 23
+GPIO_ECHO    = 24
 
-print("Ultrasonic Measurement")
+# Speed of sound in cm/s at temperature
+temperature = 20
+speedSound = 33100 + (0.6*temperature)
+
+#Hello message
+print("Ultrasonic Measurement: sonar.py")
+print("Speed of sound is",speedSound/100,"m/s at ",temperature,"deg")
 
 # Set pins as output and input
 GPIO.setup(GPIO_TRIGGER,GPIO.OUT)  # Trigger
@@ -71,20 +98,26 @@ GPIO.setup(GPIO_ECHO,GPIO.IN)      # Echo
 # Set trigger to False (Low)
 GPIO.output(GPIO_TRIGGER, False)
 
+# Allow module to settle
+# time.sleep(1)
+
 # Wrap main content in a try block so we can
 # catch the user pressing CTRL-C and run the
 # GPIO cleanup function. This will also prevent
 # the user seeing lots of unnecessary error
 # messages.
 try:
-
-  while True:
-
-    distance = measure_average()
-    print "Distance : %.1f" % distance
-    time.sleep(1)
+    min = 1000
+    while True:
+        distance = measure_average()
+        centimeters = distance
+        print("Distance : {0:5.1f}".format(distance), "cm")
+        #print "Distance : %.1f" % distance
+        if centimeters < min:
+            min = centimeters
+        time.sleep(1)
 
 except KeyboardInterrupt:
-  # User pressed CTRL-C
+    # User pressed CTRL-C
   # Reset GPIO settings
   GPIO.cleanup()
