4348cd3612417e8cf2cecf2a3f0720119970356a
= <=>
2023-02-23 21:41:57 -0800

    added multi-led-off functions

--- led.py
+++ led.py
@@ -6,6 +6,15 @@ def led_on(PIN):
 def led_off(PIN):
     GPIO.output(PIN, GPIO.LOW)
 
+def led_off(PIN1, PIN2):
+    GPIO.output(PIN1, GPIO.LOW)
+    GPIO.output(PIN2, GPIO.LOW)
+
+def led_off(PIN1, PIN2, PIN3):
+    GPIO.output(PIN1, GPIO.LOW)
+    GPIO.output(PIN2, GPIO.LOW)
+    GPIO.output(PIN3, GPIO.LOW)
+
 def blink_led ():
     print("Waiting for 1 second")
     sleep(1)
